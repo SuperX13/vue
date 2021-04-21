37a469d2140826f944a4b49309af3db653d7d7ff
yongxin <965224591@qq.com>
2021-04-21 22:38:12 +0800

    vue+springboot测试

---
+++ src/views/Show.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>姓名</td>
+                <td>性别</td>
+            </tr>
+            <tr v-for="user in users">
+                <td>{{user.id}}</td>
+                <td>{{user.name}}</td>
+                <td>{{user.sex}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+    export default {
+        name: "Show",
+        data(){
+            return{
+                users:[
+                    {
+                        id:"",
+                        name:"",
+                        sex:"",
+                    }
+                ]
+            }
+        },
+        created() {
+            const _this=this
+            axios.get('http://localhost:8888/user/showAll').then(function (resp) {
+                _this.users=resp.data
+            })
+        }
+    }
+</script>
+<style scoped>
+</style>
